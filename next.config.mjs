51ab2c4c3dd5bb62fabddb3db9bc172fb34fd37e
Varun Panchal <varunpanchal752@gmail.com>
2024-07-10 23:12:23 +0530

    Final

---
+++ next.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        domains: [
+            "iukousadrralsxhtvjkt.supabase.co"
+        ]
+    }
+};
+
+export default nextConfig;
